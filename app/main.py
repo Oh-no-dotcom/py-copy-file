f1366fdf27b90a676b43435884e5730b8a184e41
Dmytro Shulha <dimonchik0218@gmail.com>
2026-03-15 10:24:57 +0200

    task copy_file done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if not (len(parts) >= 3 and parts[0] == "cp"):
+        return
+    file_name = parts[1]
+    new_file_name = parts[2]
+    if file_name == new_file_name:
+        return
+    try:
+        with open(file_name, "r") as file_in:
+            with open(new_file_name, "w") as file_out:
+                content = file_in.read()
+                file_out.write(content)
+    except FileNotFoundError:
+        return
